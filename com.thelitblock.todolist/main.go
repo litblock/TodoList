aeaecd2ced511ad05c94be9a41b0f01863ffdaba
litblock <jonathanpeng2017@gmail.com>
2024-07-30 13:13:50 -0400

    add and list

--- com.thelitblock.todolist/main.go
+++ com.thelitblock.todolist/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"encoding/json"
 	"fmt"
 	"os"
+	"strings"
 )
 
 type Task struct {
@@ -22,9 +24,32 @@ const file = "tasks.json"
 func main() {
 	loadList := loadTodoList()
 	fmt.Println("Welcome to the Todo list app!")
-
-	printCommands()
 	fmt.Println(loadList)
+	for {
+		printCommands()
+		choice := getUserInput("Enter your choice: ")
+		switch choice {
+		case "list":
+			listTasks(loadList)
+		case "add":
+			addTask(&loadList)
+		case "complete":
+			fmt.Println("You chose complete")
+		case "delete":
+			fmt.Println("You chose delete")
+		case "quit":
+			fmt.Println("You chose quit")
+		default:
+			fmt.Println("Invalid choice")
+		}
+	}
+}
+
+func getUserInput(prompt string) string {
+	fmt.Print(prompt)
+	reader := bufio.NewReader(os.Stdin)
+	input, _ := reader.ReadString('\n')
+	return strings.TrimSpace(input)
 }
 
 func printCommands() {
@@ -49,4 +74,29 @@ func loadTodoList() TodoList {
 		return TodoList{}
 	}
 	return todoList
+}
+
+func addTask(todoList *TodoList) {
+	title := getUserInput("Enter task title: ")
+	newTask := Task{
+		ID:        len(todoList.Tasks) + 1,
+		Title:     title,
+		Completed: false,
+	}
+	todoList.Tasks = append(todoList.Tasks, newTask)
+	fmt.Println("Task added successfully!")
+}
+
+func listTasks(todoList TodoList) {
+	if len(todoList.Tasks) == 0 {
+		fmt.Println("No tasks found.")
+		return
+	}
+	for _, task := range todoList.Tasks {
+		status := "[ ]"
+		if task.Completed {
+			status = "[x]"
+		}
+		fmt.Printf("%d. %s %s\n", task.ID, status, task.Title)
+	}
 }
